10078f805b2cb3c77e280d7aedebe84f4ff4016c
Ghanashyam Aryal <anjanaryal13@gmail.com>
2025-08-29 10:39:57 +0545

    feat: add login page

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="bottom-0 left-0 right-0 z-30 bg-white">
+      <div className="mx-auto max-w-7xl px-4 py-3 text-center text-xs text-gray-600">
+        © {new Date().getFullYear()} Aryal Store.
+      </div>
+    </footer>
+  );
+}
